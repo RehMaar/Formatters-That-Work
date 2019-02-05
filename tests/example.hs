b9190c13ba84da1b335b863cc37e222adfea0bad
Maria Kuklina <mkdirwtf@gmail.com>
2019-02-06 02:33:43 +0300

    fix example

--- tests/example.hs
+++ tests/example.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TupleSections #-}
-
 module Test where
 
 import Data.List
@@ -8,6 +6,8 @@ import qualified Data.Map as M
 
 main :: IO ()
 main = interact go
+
+go :: String -> String
+go = show . reverse . sortOn snd . M.toList . M.fromListWith (+) . map (swap . (,) 1)
   where
-    go :: String -> String
-    go = show . reverse . sortOn snd . M.toList . M.fromListWith (+) . map (, 1)
+    swap = (,) <$> fst <*> snd
